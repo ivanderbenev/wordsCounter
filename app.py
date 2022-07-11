ef009183da3ab67638881ec7fac460ae6d3d38c7
ivanderbenev <ivan.derbenev@nottingham.ac.uk>
2022-07-12 00:35:05 +0100

    commit app.py

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask
+import urllib3
 
+http = urllib3.PoolManager()
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    response = http.request("GET", "https://www.bbc.co.uk", verify=False)
+    return response.status
 
 
 if __name__ == '__main__':
