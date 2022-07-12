7573ed07cbe99563f928dd9de69b5e222ca28cf4
ivanderbenev <ivan.derbenev@nottingham.ac.uk>
2022-07-12 13:51:59 +0100

    commit app.py

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 from flask import Flask
 import requests
+from html.parser import HTMLParser
 
 app = Flask(__name__)
 
+class MyHTMLParser(HTMLParser):
+    def handle_data(self, data):
+        return data
 
 @app.route('/')
 def hello_world():  # put application's code here
     response = requests.get("https://www.bbc.co.uk/")
+    parser = MyHTMLParser()
+    parser.feed(response.text)
     return str(response.text)
 
 
