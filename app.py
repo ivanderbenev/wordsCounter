5237ddfb3cbc1dc614f8e73ad7bb1c18f4fb8d33
ivanderbenev <ivan.derbenev@nottingham.ac.uk>
2022-07-12 18:22:44 +0100

    commit app.py

--- app.py
+++ app.py
@@ -1,19 +1,17 @@
 from flask import Flask
 import requests
+from bs4 import BeautifulSoup
 from html.parser import HTMLParser
 
 app = Flask(__name__)
 
-class MyHTMLParser(HTMLParser):
-    def handle_data(self, data):
-        return data
 
 @app.route('/')
 def hello_world():  # put application's code here
     response = requests.get("https://www.bbc.co.uk/")
-    parser = MyHTMLParser()
-    parser.feed(response.text)
-    return str(response.text)
+    html_source = response.text
+    soup = BeautifulSoup(html_source, 'html.parser')
+    return soup.get_text()
 
 
 if __name__ == '__main__':
