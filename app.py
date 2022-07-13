44c39daf038b87a12fe94baf6319a83cd1d674a1
ivanderbenev <ivan.derbenev@nottingham.ac.uk>
2022-07-13 21:14:35 +0100

    commit app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, request
-import requests, re
+from flask import Flask, request, render_template, abort
+import requests
+import re
 from bs4 import BeautifulSoup
+from werkzeug.exceptions import HTTPException
 
 app = Flask(__name__)
 
@@ -8,6 +10,8 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def count_words_from_url():
     url = request.args.get('url', 0, type=str)
+    if url is 0:
+        abort(400)
     response = requests.get(url)
     html_source = response.text  # html code from url
     soup = BeautifulSoup(html_source, 'html.parser')
@@ -28,8 +32,16 @@ def count_words_from_url():
         word_counter += "<tr><td>" + word + ":</td><td>" + str(number) + "</td></tr>"
 
     word_counter = "<table border=0>" + word_counter + "</table>"
-    return word_counter
+    return render_template(word_counter)
 
+@app.errorhandler(Exception)
+def handle_exception(e):
+    # HTTP errors
+    if isinstance(e, HTTPException):
+        return "HTTP error"
+
+    # non-HTTP errors
+    return "Incorrect URL"
 
 if __name__ == '__main__':
     app.run()
